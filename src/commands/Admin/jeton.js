5a22ef03449537e37715c3f2d075c5970421cd81
bekX0 <hikmetberkin@gmail.com>
2022-10-04 18:06:24 +0300

    modified:   src/commands/Admin/jeton.js

--- src/commands/Admin/jeton.js
+++ src/commands/Admin/jeton.js
@@ -15,7 +15,7 @@ export default{
         const value = parseInt(args[2])
         const data = await database.fetch(member.id)
         const balance = data.balance
-        
+        if(value<0) return message.reply({embeds:[embed(``, "Geçersiz Kullanım", "RED")]})
         if(args.length<3){
             return ({embeds:[embed(``, "Eksik Argüman", "RED")]})
         }
@@ -25,7 +25,11 @@ export default{
         }
         else if(args[0]==='al'){
             await database.update(member.id, {balance: balance - value})
-            message.reply({embeds:[embed(`<@${member.id}> adlı kişiye ${balance} Kasaba Jetonu Verildi!\n Yeni Bakiyesi :${balance-value}`, "FIRRR! PARALAR SAÇILDI", "#cc33ff")]}) 
+            message.reply({embeds:[embed(`<@${member.id}> adlı kişiden ${balance} Kasaba Jetonu Alındı!\n Yeni Bakiyesi :${balance-value}`, "VEGILENDIN!", "#cc33ff")]}) 
+        }
+        else if(args[0]==='belirle'){
+            await database.update(member.id, {balance: value})
+            message.reply({embeds:[embed(`<@${member.id}> adlı kişiye ${balance} Kasaba Jetonu Belirlendi!\n Yeni Bakiyesi :${value}`, ":))", "#cc33ff")]}) 
         }
         else{
             message.reply({embeds:[embed(``, "HATALI KULLANIM", "RED")]}) 
